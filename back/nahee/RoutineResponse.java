b7470fc3e2a7e06faa159e8fb1c5627f2c8fb9f6
naheekim <165535476+nonopenah@users.noreply.github.com>
2025-05-23 19:46:08 +0900

    Create RoutineResponse.java

---
+++ back/nahee/RoutineResponse.java
@@ -0,0 +1,20 @@
+package com.example.routine;
+
+import java.util.List;
+
+public class RoutineResponse {
+
+    private List<RoutineCard> routines;
+
+    public RoutineResponse(List<RoutineCard> routines) {
+        this.routines = routines;
+    }
+
+    public List<RoutineCard> getRoutines() {
+        return routines;
+    }
+
+    public void setRoutines(List<RoutineCard> routines) {
+        this.routines = routines;
+    }
+}
